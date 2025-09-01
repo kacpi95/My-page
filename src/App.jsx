67cc9c73cfd0ae4b970079a732ef8cd86bb286e2
Kacper <kacpergrzywacz2@gmail.com>
2025-09-01 22:24:40 +0200

    Update Route in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutPage from "./components/views/AboutPage/AboutPage";
 import HomePage from "./components/views/HomePage/HomePage";
+import ContactPage from "./components/views/ContactPage/ContactPage";
+import ProjectPage from "./components/views/ProjectPage";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route exact path={"/"} component={HomePage} />
-				<Route exact path={"/about"} component={AboutPage} />
+				<Route exact path='/' component={HomePage} />
+				<Route exact path='/about' component={AboutPage} />
+				<Route exact path='/contact' component={ContactPage} />
+				<Route exact path='/project' component={ProjectPage} />
+				<Route exact path='*' component={NoPage} />
 			</Routes>
 		</BrowserRouter>
 	);
