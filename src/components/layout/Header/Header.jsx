a0c7e6dad0fe1dd3c952a35dd2750e30557c1d72
Kacper <kacpergrzywacz2@gmail.com>
2025-09-04 22:33:11 +0200

    add: structure Header.jsx

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,3 +1,17 @@
+import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
+
 export default function Header() {
-	return;
+	return (
+		<section className={styles.header}>
+			<Link to={"/"}>
+				<img src='' alt='logo' />
+			</Link>
+			<nav className={styles.navigation}>
+				<Link to={"/about"}>O mnie</Link>
+				<Link to={"/contact"}>Kontakt</Link>
+				<Link to={"/project"}>Projekty</Link>
+			</nav>
+		</section>
+	);
 }
