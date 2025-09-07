be2e59f024a81ebf7041d8679f43813a8398428f
Kacper <kacpergrzywacz2@gmail.com>
2025-09-07 09:06:00 +0200

    add: structure in AboutPage

--- src/components/views/AboutPage/AboutPage.jsx
+++ src/components/views/AboutPage/AboutPage.jsx
@@ -1,3 +1,30 @@
 export default function AboutPage() {
-	return;
+	return (
+		<section id='about'>
+			<div class='container'>
+				<div class='wrapper-about'>
+					<h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, libero!</h2>
+					<p class='description'>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut veritatis maxime in distinctio aperiam esse ipsa minus! Eum quisquam est vero, natus delectus voluptatum molestias ea porro expedita! Corrupti, nam.
+					</p>
+					<div class='images-row'>
+						<img src='' alt='' />
+						<img
+							src=''
+							alt=''
+						/>
+					</div>
+					<div class='buttons-row'>
+						<button></button>
+						<button></button>
+						<button></button>
+					</div>
+				</div>
+				<div class='image-profile'>
+					<img src='' alt='' />
+					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, velit.</p>
+				</div>
+			</div>
+		</section>
+	);
 }
