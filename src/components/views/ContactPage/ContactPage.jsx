4b5fd52a52fce0b17876c0eb82064af5cb719370
Kacper <kacpergrzywacz2@gmail.com>
2025-09-10 22:08:22 +0200

    add structure ContactPage

--- src/components/views/ContactPage/ContactPage.jsx
+++ src/components/views/ContactPage/ContactPage.jsx
@@ -1,3 +1,28 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./ContactPage.module.scss";
+import Header from "../../layout/Header/Header";
+
 export default function ContactPage() {
-	return;
+	return (
+		<>
+			<Header />
+			<section id='contact'>
+				<div>Wybierz rodzaj kotaktu</div>
+				<div className={styles.contactChoose}>
+					<div>
+						<FontAwesomeIcon />
+						<span>Discrod</span>
+					</div>
+					<div>
+						<FontAwesomeIcon />
+						<span>Linkedin</span>
+					</div>
+					<div>
+						<FontAwesomeIcon />
+						<span>E-mail</span>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 }
