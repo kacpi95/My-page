e73dc5c8a7f37bc56544e75821ceaac05c8b64b8
Kacper <kacpergrzywacz2@gmail.com>
2025-09-12 22:10:43 +0200

    update ContactPage

--- src/components/views/ContactPage/ContactPage.jsx
+++ src/components/views/ContactPage/ContactPage.jsx
@@ -1,5 +1,10 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDiscord, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import {
+	faDiscord,
+	faLinkedin,
+	faFacebook,
+	faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
 import styles from "./ContactPage.module.scss";
 import Header from "../../layout/Header/Header";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
@@ -22,6 +27,14 @@ export default function ContactPage() {
 					<div>
 						<FontAwesomeIcon icon={faEnvelope} />
 						<span>E-mail</span>
+					</div>
+					<div>
+						<FontAwesomeIcon icon={faFacebook} />
+						<span>Facebook</span>
+					</div>
+					<div>
+						<FontAwesomeIcon icon={faWhatsapp} />
+						<span>Whatsapp</span>
 					</div>
 				</div>
 			</section>
