aee8b989858f87daf9c5ceb986af003fe969cb94
Kacper <kacpergrzywacz2@gmail.com>
2025-09-14 23:39:32 +0200

    add: structure footer

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,3 +1,11 @@
+import styles from "./Footer.module.scss";
+
 export default function Footer() {
-	return;
+	return (
+		<footer className={styles.footer}>
+			<div className={styles.footerContent}>
+				© {new Date().getFullYear()} MyPage. Kacper Grzywacz.
+			</div>
+		</footer>
+	);
 }
