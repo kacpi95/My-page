d9e5ed13cc33b437341546b5c384b366f26bf35a
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-09-16 23:41:24 +0200

    add: structur for NoPage

--- src/components/views/NoPage/NoPage.jsx
+++ src/components/views/NoPage/NoPage.jsx
@@ -1,3 +1,13 @@
+import styles from './NoPage.module.scss';
+
 export default function NoPage() {
-	return;
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.heading}>404</h1>
+      <p style={styles.text}>Ups! Strona, której szukasz, nie istnieje.</p>
+      <a href='/' style={styles.link}>
+        Wróć na stronę główną
+      </a>
+    </div>
+  );
 }
