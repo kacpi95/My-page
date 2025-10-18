21b53caa17a63440f099854e132195cdb899d044
Kacper <kacpergrzywacz2@gmail.com>
2025-10-18 22:26:08 +0200

    import Footer

--- src/components/views/HomePage/HomePage.jsx
+++ src/components/views/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import Header from "../../layout/Header/Header";
+import Footer from "../../layout/Footer/Footer";
 
 export default function HomePage() {
 	return (
 		<>
 			<Header />
+			<Footer />
 		</>
 	);
 }
