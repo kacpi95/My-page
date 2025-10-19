63663af3ad4e03aca9b44ac1e4b453631eaac00e
Kacper <kacpergrzywacz2@gmail.com>
2025-10-19 23:47:07 +0200

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import './styles/global.scss';
-import { createRoot } from 'react-dom/client';
+import "./styles/global.scss";
+import { createRoot } from "react-dom/client";
 
-import App from './App.jsx';
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById("root")).render(<App />);
