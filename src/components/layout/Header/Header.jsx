67c46420a7997bf391f33b720302e6628d30ee7e
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-11-02 14:03:12 +0100

    update: Header and styles Header

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,25 +1,27 @@
-import { Link } from "react-router-dom";
-import styles from "./Header.module.scss";
+import { Link } from 'react-router-dom';
+import styles from './Header.module.scss';
 
 export default function Header() {
-	return (
-		<section className={styles.header} id='header'>
-			<Link to={"/"}>
-				<img src='' alt='logo' className={styles.logo} />
-			</Link>
-			<nav className={styles.navigation}>
-				<ul>
-					<li>
-						<Link to={"/about"}>O mnie</Link>
-					</li>
-					<li>
-						<Link to={"/contact"}>Kontakt</Link>
-					</li>
-					<li>
-						<Link to={"/project"}>Projekty</Link>
-					</li>
-				</ul>
-			</nav>
-		</section>
-	);
+  return (
+    <header className={styles.header} id='header'>
+      <div className={styles.container}>
+        <Link to={'/'} className={styles.logo}>
+          kacpi.dev
+        </Link>
+        <nav className={styles.navigation}>
+          <ul>
+            <li>
+              <Link to={'/about'}>About</Link>
+            </li>
+            <li>
+              <Link to={'/project'}>Projects</Link>
+            </li>
+            <li>
+              <Link to={'/contact'}>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
