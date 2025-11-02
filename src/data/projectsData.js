2744d1c47e2512eae3c00cfcdf8b1fcd92f32134
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-11-02 15:04:58 +0100

    add: data - projects

---
+++ src/data/projectsData.js
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    title: 'project 1',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at pulvinar massa',
+    technologies: ['React', 'Express', 'i18next'],
+    image: './images/project1.png',
+    link: 'https://github.com/kacpi95',
+  },
+  {
+    title: 'project 2',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at pulvinar massa',
+    technologies: ['JS', 'Express', 'i18next'],
+    image: './images/project2.png',
+    link: 'https://github.com/kacpi95',
+  },
+  {
+    title: 'project 3',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at pulvinar massa',
+    technologies: ['React', 'Node', 'MongoDB'],
+    image: './images/project3.png',
+    link: 'https://github.com/kacpi95',
+  },
+];
+
+export default projects;
