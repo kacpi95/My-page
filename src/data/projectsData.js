d993297304b1e458d48edd999168ab45d5872a2a
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-11-02 15:21:41 +0100

    update: data - projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,24 +1,30 @@
 const projects = [
   {
     title: 'project 1',
-    description:
+    shortDescription:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at pulvinar massa',
+    longDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sagittis varius consectetur. Nulla egestas urna id tincidunt ullamcorper. Pellentesque facilisis malesuada ullamcorper. Praesent in porta orci. Etiam tempor sapien a felis malesuada, id fringilla leo semper. Fusce facilisis, nisl vel imperdiet imperdiet, enim turpis aliquam augue, sed laoreet nisi neque eget est. Pellentesque sed justo ut risus gravida consectetur. Aenean facilisis, odio sit amet facilisis ultrices, enim enim maximus quam, in aliquet eros ex cursus ex. Cras iaculis nisl quis ipsum ornare, eget lacinia leo pharetra. Nunc ultrices congue urna a eleifend. Fusce at placerat urna. Praesent venenatis turpis vitae lacus lacinia placerat.',
     technologies: ['React', 'Express', 'i18next'],
     image: './images/project1.png',
     link: 'https://github.com/kacpi95',
   },
   {
     title: 'project 2',
-    description:
+    shortDescription:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at pulvinar massa',
+    longDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sagittis varius consectetur. Nulla egestas urna id tincidunt ullamcorper. Pellentesque facilisis malesuada ullamcorper. Praesent in porta orci. Etiam tempor sapien a felis malesuada, id fringilla leo semper. Fusce facilisis, nisl vel imperdiet imperdiet, enim turpis aliquam augue, sed laoreet nisi neque eget est. Pellentesque sed justo ut risus gravida consectetur. Aenean facilisis, odio sit amet facilisis ultrices, enim enim maximus quam, in aliquet eros ex cursus ex. Cras iaculis nisl quis ipsum ornare, eget lacinia leo pharetra. Nunc ultrices congue urna a eleifend. Fusce at placerat urna. Praesent venenatis turpis vitae lacus lacinia placerat.',
     technologies: ['JS', 'Express', 'i18next'],
     image: './images/project2.png',
     link: 'https://github.com/kacpi95',
   },
   {
     title: 'project 3',
-    description:
+    shortDescription:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at pulvinar massa',
+    longDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sagittis varius consectetur. Nulla egestas urna id tincidunt ullamcorper. Pellentesque facilisis malesuada ullamcorper. Praesent in porta orci. Etiam tempor sapien a felis malesuada, id fringilla leo semper. Fusce facilisis, nisl vel imperdiet imperdiet, enim turpis aliquam augue, sed laoreet nisi neque eget est. Pellentesque sed justo ut risus gravida consectetur. Aenean facilisis, odio sit amet facilisis ultrices, enim enim maximus quam, in aliquet eros ex cursus ex. Cras iaculis nisl quis ipsum ornare, eget lacinia leo pharetra. Nunc ultrices congue urna a eleifend. Fusce at placerat urna. Praesent venenatis turpis vitae lacus lacinia placerat.',
     technologies: ['React', 'Node', 'MongoDB'],
     image: './images/project3.png',
     link: 'https://github.com/kacpi95',
