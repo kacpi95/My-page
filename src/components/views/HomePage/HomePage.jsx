7743c01586649dc2d2ef21740ed8f182c9057817
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-11-02 16:33:38 +0100

    fix structure folder

--- src/components/views/HomePage/HomePage.jsx
+++ src/components/views/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import About from '../../features/About/About.jsx';
-import ProjectsList from '../../features/About/ProjectsList/ProjectsList.jsx';
+import ProjectsList from '../../features/ProjectsList/ProjectsList.jsx';
 
 export default function HomePage() {
   return (
