ac654ed82318ba12226a9579289abb4a1ec92b6b
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-11-02 17:40:58 +0100

    update: footer component

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,11 +1,30 @@
-import styles from "./Footer.module.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
+import styles from './Footer.module.scss';
 
 export default function Footer() {
-	return (
-		<footer className={styles.footer}>
-			<div className={styles.footerContent}>
-				© {new Date().getFullYear()} MyPage. Kacper Grzywacz.
-			</div>
-		</footer>
-	);
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <p>
+          © {new Date().getFullYear()} Kacper Grzywacz. All rights reserved.
+        </p>
+        <div className={styles.socials}>
+          <a href='https://github.com/kacpi95' target='_blank'>
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a href='#'>
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a href='#'>
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
