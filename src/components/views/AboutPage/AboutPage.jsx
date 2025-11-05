324c81744bf86d22f7eefb809706188dd49292a1
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-11-05 16:39:08 +0100

    add: structure to AboutPage and styles

--- src/components/views/AboutPage/AboutPage.jsx
+++ src/components/views/AboutPage/AboutPage.jsx
@@ -1,5 +1,14 @@
-// import styles from './AboutPage.module.scss';
+import styles from './AboutPage.module.scss';
+import AboutIntro from '../../features/AboutIntro/AboutIntro';
 
 export default function AboutPage() {
-  return;
+  return (
+    <section className={styles.about}>
+      <div className={styles.container}>
+        <div className={styles.inner}>
+          <AboutIntro />
+        </div>
+      </div>
+    </section>
+  );
 }
