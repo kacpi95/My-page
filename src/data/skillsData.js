6bed914f02db425d64f7f769c9d4f39298dc7135
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-11-05 17:13:17 +0100

    add: file data skills

---
+++ src/data/skillsData.js
@@ -0,0 +1,13 @@
+const skills = {
+  frontend: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'TypeScript',
+    'React',
+    'React-Native',
+  ],
+  backend: ['Node', 'Express', 'NestJS', 'Websocket'],
+  database: ['MongoDB', 'MySQL', 'Prisma'],
+};
+export default skills;
