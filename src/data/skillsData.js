40a90f3f2e32dec8eda530dc20f8ffbe4ca12ee1
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-11-05 18:03:36 +0100

    update: data skills

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,13 +1,24 @@
 const skills = {
   frontend: [
-    'HTML',
-    'CSS',
-    'JavaScript',
-    'TypeScript',
-    'React',
-    'React-Native',
+    { name: 'HTML', level: 'intermediate' },
+    { name: 'CSS', level: 'intermediate' },
+    { name: 'SCSS', level: 'intermediate' },
+    { name: 'JavaScript', level: 'intermediate' },
+    { name: 'TypeScript', level: 'beginner' },
+    { name: 'React', level: 'intermediate' },
+    { name: 'React-Native', level: 'intermediate' },
+    { name: 'Webpack', level: 'beginner' },
   ],
-  backend: ['Node', 'Express', 'NestJS', 'Websocket'],
-  database: ['MongoDB', 'MySQL', 'Prisma'],
+  backend: [
+    { name: 'Node', level: 'intermediate' },
+    { name: 'Express', level: 'intermediate' },
+    { name: 'NestJS', level: 'beginner' },
+    { name: 'Websocket', level: 'intermediate' },
+  ],
+  database: [
+    { name: 'MongoDB', level: 'intermediate' },
+    { name: 'MySQL', level: 'intermediate' },
+    { name: 'Prisma', level: 'beginner' },
+  ],
 };
 export default skills;
