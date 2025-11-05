ecf5054496f425caa3a79d0e8e889adc3d2f3a3d
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-11-05 18:52:15 +0100

    add: styles to ContactPage component

--- src/components/views/ContactPage/ContactPage.jsx
+++ src/components/views/ContactPage/ContactPage.jsx
@@ -10,12 +10,14 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 export default function ContactPage() {
   return (
     <section className={styles.contactSection}>
-      <h2>Choose a way to contact me</h2>
+      <h2 className={styles.title}>
+        <span>Choose</span> a way to <span>contact me</span>
+      </h2>
 
       <div className={styles.contactList}>
         <ContactItem icon={faDiscord} label='Discord' />
         <ContactItem icon={faLinkedin} label='LinkedIn' />
-        <ContactItem icon={faEnvelope} lable='E-mail' />
+        <ContactItem icon={faEnvelope} label='E-mail' />
         <ContactItem icon={faWhatsapp} label='Whatsapp' />
       </div>
     </section>
